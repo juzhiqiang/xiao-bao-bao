0ddeea325e5b3cdda9a78541a7073b3914c94f80
miss you <1020814597@qq.com>
2025-08-10 01:46:02 +0800

    Add Apollo Provider to main.tsx for GraphQL integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ApolloProvider } from '@apollo/client'
 import App from './App.tsx'
+import apolloClient from './lib/apollo.ts'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 )
