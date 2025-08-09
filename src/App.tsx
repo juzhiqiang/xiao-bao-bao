7bf977fa06075e56cf7bc23adc381d06e0115989
miss you <1020814597@qq.com>
2025-08-09 20:03:28 +0800

    Add App.tsx main component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import XiaoBaoBaoChat from './components/XiaoBaoBaoChat';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <XiaoBaoBaoChat />
+    </div>
+  );
+}
+
+export default App;
