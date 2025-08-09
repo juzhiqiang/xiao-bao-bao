d063e72345fed48dce70fba22f5bfff0666f9371
miss you <1020814597@qq.com>
2025-08-10 05:43:47 +0800

    Add debug toggle to switch between chat and GraphQL test components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import { ApolloProvider } from '@apollo/client';
 import XiaoBaoBaoChat from './components/XiaoBaoBaoChat';
+import GraphQLTest from './components/GraphQLTest';
 import apolloClient from './lib/apollo';
 import './index.css';
 
 export default function App() {
+  const [showTest, setShowTest] = useState(false);
+
   return (
     <ApolloProvider client={apolloClient}>
       <div className="App">
-        <XiaoBaoBaoChat />
+        {/* Debug Toggle */}
+        <div className="fixed top-4 right-4 z-50">
+          <button
+            onClick={() => setShowTest(!showTest)}
+            className="px-3 py-1 bg-gray-800 text-white text-sm rounded hover:bg-gray-700"
+          >
+            {showTest ? '返回聊天' : '调试模式'}
+          </button>
+        </div>
+        
+        {showTest ? <GraphQLTest /> : <XiaoBaoBaoChat />}
       </div>
     </ApolloProvider>
   );
