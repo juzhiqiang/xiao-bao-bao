49b31a6f512d04e955bc7204d482be1c4cd1aab3
miss you <1020814597@qq.com>
2025-08-10 10:27:00 +0800

    更新组件导出，包含新的流式聊天组件

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,5 @@
 export { default as XiaoBaoBaoChat } from './XiaoBaoBaoChat';
+export { default as XiaoBaoBaoStreamingChat } from './XiaoBaoBaoStreamingChat';
+export { default as DirectAPITest } from './DirectAPITest';
+export { default as GraphQLTest } from './GraphQLTest';
+export { default as SimpleGraphQLTest } from './SimpleGraphQLTest';
