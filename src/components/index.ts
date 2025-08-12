b5b1ef83677535d365720b1acb7fa235e18e635b
miss you <1020814597@qq.com>
2025-08-12 23:28:12 +0800

    更新组件导出 - 添加LoadingScreen导出

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as XiaoBaoBaoChat } from './XiaoBaoBaoChat';
 export { default as XiaoBaoBaoStreamingChat } from './XiaoBaoBaoStreamingChat';
 export { default as ContractReviewChat } from './ContractReviewChat';
 export { default as ErrorBoundary } from './ErrorBoundary';
+export { default as LoadingScreen } from './LoadingScreen';
