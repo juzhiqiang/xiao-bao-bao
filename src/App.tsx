15e87edc7b737349d4e50fcdfebfa0b92d95aa65
miss you <1020814597@qq.com>
2025-08-12 23:16:50 +0800

    添加路由配置 - 设置主页面和合同审核页面的路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
 import XiaoBaoBaoStreamingChat from './components/XiaoBaoBaoStreamingChat';
+import ContractReviewChat from './components/ContractReviewChat';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <XiaoBaoBaoStreamingChat />
+      <Routes>
+        <Route path="/" element={<XiaoBaoBaoStreamingChat />} />
+        <Route path="/contract-review" element={<ContractReviewChat />} />
+      </Routes>
     </div>
   );
 }
