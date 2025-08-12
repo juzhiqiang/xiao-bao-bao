f2d38f152671ada032767afebe6d3eb32d45e6ca
miss you <1020814597@qq.com>
2025-08-12 23:17:35 +0800

    更新main.tsx - 添加错误边界和根元素检查

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,24 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ApolloProvider } from '@apollo/client'
 import App from './App.tsx'
+import ErrorBoundary from './components/ErrorBoundary.tsx'
 import apolloClient from './lib/apollo.ts'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
+// 确保root元素存在
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+
+createRoot(rootElement).render(
   <StrictMode>
-    <BrowserRouter basename="/xiao-bao-bao">
-      <ApolloProvider client={apolloClient}>
-        <App />
-      </ApolloProvider>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter basename="/xiao-bao-bao">
+        <ApolloProvider client={apolloClient}>
+          <App />
+        </ApolloProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
   </StrictMode>,
 )
