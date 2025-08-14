39e4e530209c130762269a0369d8bc75a7b4a4c3
miss you <1020814597@qq.com>
2025-08-14 21:40:23 +0800

    Add TravelPlanningChat to component exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as XiaoBaoBaoChat } from './XiaoBaoBaoChat';
 export { default as XiaoBaoBaoStreamingChat } from './XiaoBaoBaoStreamingChat';
 export { default as ContractReviewChat } from './ContractReviewChat';
+export { default as TravelPlanningChat } from './TravelPlanningChat';
 export { default as ErrorBoundary } from './ErrorBoundary';
 export { default as LoadingScreen } from './LoadingScreen';
